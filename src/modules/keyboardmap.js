2d7235223470c2a9c7b07381beba2ef5e23a673b
LidaChk <lvglazko@gmail.com>
2022-05-08 23:30:58 +0300

    feat: backspace and del

--- src/modules/keyboardmap.js
+++ src/modules/keyboardmap.js
@@ -1,23 +1,18 @@
 const winKey = `<span class="material-symbols-outlined">
 keyboard_command_key
 </span>`;
-const ArrowLeft = `<span class="material-symbols-outlined">
-arrow_back
-</span>`;
-const ArrowRight = `<span class="material-symbols-outlined">
-arrow_forward
-</span>`;
-const ArrowDown = `<span class="material-symbols-outlined">arrow_downward
-</span>`;
-const ArrowUp = `<span class="material-symbols-outlined">
-arrow_upward
-</span>`;
+const ArrowLeft = '◄';
+const ArrowRight = '►';
+const ArrowUp = '▲';
+const ArrowDown = '▼';
+const Shift = '&#8679; Shift';
+const Enter = 'Enter&#8239;<big>&#8629;</big>';
 /* TODO: поменять иконки шифтови ентер и бекспейс */
 const keyboardMap = {
-  line1: ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace:Func:2:&#x2190;Backspace', 'Delete:Func:1:Del'],
-  line2: ['Tab:Func:1.5:Tab&#x2194;', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash:key:1.5', 'None'],
-  line3: ['CapsLock:Func:1.75:CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter:Func:2.25:Enter&#x21B5;', 'None'],
-  line4: ['ShiftLeft:Func:2.25:&#x21D1;Shift', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight:Func:1.75:&#x21D1;Shift', `ArrowUp:Arrow:1:${ArrowUp}`, 'None'],
+  line1: ['Backquote:key', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace:Func:2:<big>⟵</big>&#8239;Backspace', 'Delete:Func:1:Del'],
+  line2: ['Tab:Func:1.5:Tab&#8239;<big>&#11120;</big>', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash:key:1.5', 'None:FunFunc'],
+  line3: ['CapsLock:Func:1.75:CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', `Enter:Func:2.25:${Enter}`, 'None:FunFunc'],
+  line4: [`ShiftLeft:Func:2.25:${Shift}`, 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma:key', 'Period:key', 'Slash:key', `ShiftRight:Func:1.75:${Shift}`, `ArrowUp:Arrow:1:${ArrowUp}`, 'None:FunFunc'],
   line5: ['ControlLeft:Func:1.5:Ctrl', `MetaLeft:Func:1:${winKey}`, 'AltLeft:Func:1:Alt', 'Space:space:7.5', 'AltRight:Func:1:Alt', 'ControlRight:Func:1:Ctrl', `ArrowLeft:Arrow:1:${ArrowLeft}`, `ArrowDown:Arrow:1:${ArrowDown}`, `ArrowRight:Arrow:1:${ArrowRight}`],
 };
 
--- src/modules/keyborad.js
+++ src/modules/keyborad.js
@@ -7,12 +7,13 @@ export default class Keyboard {
   constructor() {
     this.element = document.createElement('div');
     this.element.classList.add('keyborad-container');
-    this.btns = [];
+    this.btns = {};
     this.capsed = false;
     this.shifted = false;
     this.alted = false;
     this.lang = 'Eng';
     this.langs = ['Eng', 'Rus'];
+    this.mousePressedCode = '';
 
     Object.keys(keyboardMap).forEach((key) => {
       const line = document.createElement('div');
@@ -21,7 +22,7 @@ export default class Keyboard {
       keyboardMap[key].forEach((k) => {
         const btnKey = new Button(...k.split(':'));
         line.appendChild(btnKey.element);
-        this.btns.push(btnKey);
+        this.btns[btnKey.code] = btnKey;
       });
       this.element.appendChild(line);
     });
@@ -31,28 +32,36 @@ export default class Keyboard {
 
     document.querySelector('.components').appendChild(this.element);
     this.addEvents();
-    console.log(this.btns);
   }
 
   addEvents() {
     document.addEventListener('click', () => { this.textarea.focus(); });
 
-    this.element.addEventListener('click', (e) => {
+    this.element.addEventListener('mousedown', (e) => {
       const key = e.target.closest('.btn');
       if (key) {
+        this.mousePressedCode = key.id;
         this.activatekey(key.id);
-        this.deactivatekey(key.id);
+      }
+    });
+    document.addEventListener('mouseup', (e) => {
+      const key = e.target.closest('.btn');
+      if (this.mousePressedCode.slice(0, 5) === 'Shift' && (!key || key.id !== this.mousePressedCode)) {
+        this.shifted = true;
+      } else if (this.mousePressedCode !== '') {
+        this.deactivatekey(this.mousePressedCode);
+        if (this.btns.ShiftLeft.isPressed) this.deactivatekey(this.btns.ShiftLeft.code);
+        if (this.btns.ShiftRight.isPressed) this.deactivatekey(this.btns.ShiftRight.code);
+        this.mousePressedCode = '';
       }
     });
 
     this.textarea.addEventListener('keydown', (e) => {
       this.activatekey(e.code);
-
       e.preventDefault();
     });
     this.textarea.addEventListener('keyup', (e) => {
       this.deactivatekey(e.code);
-
       e.preventDefault();
     });
   }
@@ -64,42 +73,59 @@ export default class Keyboard {
   }
 
   activatekey(code) {
-    const btn = this.btns.find((b) => b.code === code);
+    const btn = this.btns[code];
     if (btn) btn.element.classList.add('btn--active');
     if (code === 'CapsLock' && !btn.isPressed) {
       this.capsed = !this.capsed;
       btn.element.classList.toggle('capsed');
       btn.isPressed = true;
       this.initLang();
-    }
-    if (code.slice(0, 5) === 'Shift' && !btn.isPressed) {
+    } else if (code.slice(0, 5) === 'Shift' && !btn.isPressed) {
       this.shifted = !this.shifted;
       btn.isPressed = true;
       if (this.alted) { this.switchLang(); }
       this.initLang();
-    }
-    if (code.slice(0, 3) === 'Alt' && !btn.isPressed) {
+    } else if (code.slice(0, 3) === 'Alt' && !btn.isPressed) {
       this.alted = !this.alted;
       btn.isPressed = true;
       if (this.shifted) {
         this.switchLang();
         this.initLang();
       }
+    } else if (code === 'Enter') {
+      this.textarea.value += '\n';
+    } else if (code === 'Tab') {
+      this.textarea.value += '\t';
+    } else if (code === 'Backspace' || code === 'Delete') {
+      const start = this.textarea.selectionStart;
+      const end = this.textarea.selectionEnd;
+      const { value } = this.textarea;
+      if (end > start) {
+        console.log(end, start);
+        this.textarea.value = value.slice(0, start) + value.slice(end);
+        this.textarea.selectionEnd = start;
+      } else if (code === 'Backspace') {
+        this.textarea.value = value.slice(0, start - 1) + value.slice(end);
+        this.textarea.selectionEnd = start - 1;
+      } else if (code === 'Delete') {
+        this.textarea.value = value.slice(0, start) + value.slice(end + 1);
+        this.textarea.selectionEnd = start;
+      }
+    } else if (btn.type === 'key' || btn.type === 'Arrow') {
+      this.textarea.value += btn.label;
     }
   }
 
   deactivatekey(code) {
-    const btn = this.btns.find((b) => b.code === code);
+    const btn = this.btns[code];
     if (btn) btn.element.classList.remove('btn--active');
     if (code === 'CapsLock') {
       btn.isPressed = false;
-    }
-    if (code.slice(0, 5) === 'Shift' && btn.isPressed) {
+    } else if (code.slice(0, 5) === 'Shift' && btn.isPressed) {
       this.shifted = !this.shifted;
       btn.isPressed = false;
       this.initLang();
-    }
-    if (code.slice(0, 3) === 'Alt' && btn.isPressed) {
+    } else if (code.slice(0, 3) === 'Alt' && btn.isPressed) {
       this.alted = !this.alted;
       btn.isPressed = false;
     }
@@ -108,10 +134,9 @@ export default class Keyboard {
   initLang() {
     const lay = layouts.find((l) => l.lang === this.lang && l.shifted === this.shifted).layout;
     console.log(this.shifted);
-    this.btns.filter((b) => b.type === 'key').forEach((b) => {
-      if (lay[b.code] !== undefined) {
-        if (this.capsed && this.shifted && b.code.slice(0, 3) === 'Key') { b.changeLabel(lay[b.code].toLowerCase()); } else if (this.capsed && !this.shifted && b.code.slice(0, 3) === 'Key') { b.changeLabel(lay[b.code].toUpperCase()); } else b.changeLabel(lay[b.code]);
-      }
+    const keys = Object.keys(this.btns);
+    keys.filter((b) => this.btns[b].type === 'key').forEach((b) => {
+      if (this.capsed && this.shifted && b.slice(0, 3) === 'Key') { this.btns[b].changeLabel(lay[b].toLowerCase()); } else if (this.capsed && !this.shifted && b.slice(0, 3) === 'Key') { this.btns[b].changeLabel(lay[b].toUpperCase()); } else this.btns[b].changeLabel(lay[b]);
     });
   }
 }
